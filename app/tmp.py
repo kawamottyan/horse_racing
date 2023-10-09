9f1bae627f65d75f4c699d3cc55f5b75424345bf
kawamottyan <101444666+kawamottyan@users.noreply.github.com>
2023-10-09 17:02:15 +0900

    update tmp

--- app/tmp.py
+++ app/tmp.py
@@ -23,19 +23,21 @@
 # # venues = [li.a.text.strip() for li in soup.select('ul.RaceList_ProvinceSelect li')]
 
 import streamlit as st
-import os, sys
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-_ = installff()
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
-browser.get('http://example.com')
-st.write(browser.page_source)
+driver = get_driver()
+driver.get('http://example.com')
+
+st.code(driver.page_source)
